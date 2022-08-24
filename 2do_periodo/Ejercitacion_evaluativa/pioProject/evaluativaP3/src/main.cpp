f6fa367e8395538766bfc60f7f6ec89adbd2b5ba
Gustavo Godoy <35662736+chulkx@users.noreply.github.com>
2022-08-23 23:50:06 -0300

    carga void setup

--- 2do_periodo/Ejercitacion_evaluativa/pioProject/evaluativaP3/src/main.cpp
+++ 2do_periodo/Ejercitacion_evaluativa/pioProject/evaluativaP3/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+// int ejercicio1();
+// void ejercicio2();
+// void ejercicio3();
+// void ejercicio4();
 
+bool seguir = true;
+int led = 11;
+int potencia_led = 0;
+int control = 115;
+String pass = "2147483648";
+int ejercicio1();
+void ejercicio2();
+void ejercicio3();
+void ejercicio4();
+void ejercicio5();
+bool ejercicio6();
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(12, OUTPUT);
+  DDRD = 0b11111111;
+
+}
 void loop() {
   // put your main code here, to run repeatedly:
 }
